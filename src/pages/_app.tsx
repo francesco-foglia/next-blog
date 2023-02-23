71b14db26d8aebd2a300a222e26834d88f027ea3
Francesco Foglia <francescofogliajj@gmail.com>
2023-02-23 11:30:35 +0100

    modified:   package-lock.json modified:   package.json modified:   src/pages/_app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,7 @@
 import '@/styles/globals.css'
+import 'jquery/dist/jquery.js';
+import 'popper.js/dist/umd/popper.js';
+import 'bootstrap/dist/js/bootstrap.js';
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
