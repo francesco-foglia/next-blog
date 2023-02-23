d3622c8e91271aff98f4cde675ae9b14f4338be6
Francesco Foglia <francescofogliajj@gmail.com>
2023-02-23 14:58:54 +0100

    modified:   components/PostPreview.tsx modified:   src/styles/globals.css

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -10,15 +10,19 @@ interface PostPreviewProps {
   post: Post;
 }
 
-const PostPreview = ({ post }: PostPreviewProps) => (
-  <li key={post.id} className="col-md-6 col-lg-4">
-    <div className="card mb-3">
-      <div className="card-body">
-        <h2 className="card-title h5">{post.title}</h2>
-        <p className="card-text">{post.body}</p>
+const PostPreview: React.FC<PostPreviewProps> = ({ post }) => {
+  const firstWord = post.title[0].toUpperCase() + post.title.slice(1).split(' ')[0];
+  return (
+    <li key={post.id} className="col-md-6 col-lg-4">
+      <div className="card mb-3">
+        <img src={`https://via.placeholder.com/300?text=${firstWord}`} className="card-img-top" alt={post.title} />
+        <div className="card-body">
+          <h2 className="card-title h5">{post.title}</h2>
+          <p className="card-text">{post.body}</p>
+        </div>
       </div>
-    </div>
-  </li >
-);
+    </li>
+  );
+};
 
 export default PostPreview;
