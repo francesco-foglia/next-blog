a01d8f2fbbfdd6758f79bba3dc38ea827fe40320
Francesco Foglia <francescofogliajj@gmail.com>
2023-02-23 10:24:48 +0100

    modified:   package-lock.json modified:   package.json modified:   src/pages/posts/[id].js modified:   src/styles/globals.css

--- src/pages/posts/[id].js
+++ src/pages/posts/[id].js
@@ -6,7 +6,7 @@ export default function Post() {
   const { id } = router.query;
 
   return (
-    <div>
+    <div className="container py-5">
       {/* <h1>Post: {id}</h1> */}
       {/* <h1>Post: {JSON.stringify(router.query)}</h1> */}
       <h1>Post: {JSON.stringify(id)}</h1>
