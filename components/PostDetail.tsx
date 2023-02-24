9ad9ac17d9130178436397697a5af763da24aa1a
Francesco Foglia <francescofogliajj@gmail.com>
2023-02-24 11:35:08 +0100

    modified:   components/PostDetail.tsx

--- components/PostDetail.tsx
+++ components/PostDetail.tsx
@@ -18,16 +18,16 @@ const PostDetail: React.FC<PostDetailProps> = ({ post }) => {
   return (
     <div className="pb-5 row">
       <div className="col-lg-6">
-        <div className="post-detail-img-container my-4">
+        <div className="post-detail-img-container">
           <img src={`https://via.placeholder.com/500x500?text=${firstWord}`} alt={title} className="post-detail-img" />
         </div>
       </div>
-      <div className="col-lg-6">
+      <div className="col-lg-6 mt-3 mt-lg-0">
         <h1 className="display-4">{title}</h1>
         <p className="lead my-4">{post.body}</p>
       </div>
       <div className="col-12">
-        <Link href="/" className="btn btn-secondary back-to-posts">Back to posts</Link>
+        <Link href="/" className="btn btn-secondary back-to-posts mt-0 mt-lg-3">Back to posts</Link>
       </div>
     </div>
   );
