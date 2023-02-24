b13d920496bebee7a0d253262434a7d2712c1670
Francesco Foglia <francescofogliajj@gmail.com>
2023-02-24 10:04:45 +0100

    modified:   components/PostPreview.tsx modified:   src/styles/globals.css

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -14,7 +14,7 @@ interface PostPreviewProps {
 const PostPreview: React.FC<PostPreviewProps> = ({ post }) => {
   const firstWord = post.title[0].toUpperCase() + post.title.slice(1).split(' ')[0];
   return (
-    <li key={post.id} className="col-md-6 col-lg-4">
+    <li key={post.id} className="col-lg-6 col-xl-4 col-xxl-3">
       <div className="card mb-3">
         <img src={`https://via.placeholder.com/500?text=${firstWord}`} className="card-img-top" alt={post.title} />
         <div className="card-body">
