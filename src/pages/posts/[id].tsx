92c8760f3b28a1d639019bf736f6bb27dd244d15
Francesco Foglia <francescofogliajj@gmail.com>
2023-02-24 12:09:13 +0100

    modified:   src/pages/posts/[id].tsx

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useRouter } from "next/router";
 import BaseLayout from "src/components/layout/BaseLayout";
 import PostDetail from "src/components/PostDetail";
@@ -9,32 +9,37 @@ interface Post {
   body: string;
 }
 
-export default function Post() {
+interface GetStaticPropsParams {
+  id: string;
+}
+
+const Post = ({ post }: { post: Post }) => {
   const router = useRouter();
   const { id } = router.query;
-  const [post, setPost] = useState<Post | null>(null);
-
-  useEffect(() => {
-    const fetchPost = async () => {
-      try {
-        if (id) {
-          const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
-          const post = await response.json();
-          setPost(post);
-        }
-      } catch (err) {
-        console.log(err);
-      }
-    }
-
-    fetchPost();
-  }, [id])
 
   return (
-    <>
-      <BaseLayout>
-        {post && <PostDetail post={post} />}
-      </BaseLayout>
-    </>
+    <BaseLayout>
+      {post && <PostDetail post={post} />}
+    </BaseLayout>
   );
+}
+
+export default Post;
+
+export async function getStaticPaths() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const posts = await response.json();
+
+  const paths = posts.map((post: Post) => ({
+    params: { id: post.id.toString() },
+  }));
+
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps({ params }: { params: GetStaticPropsParams }) {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.id}`);
+  const post = await response.json();
+
+  return { props: { post } };
 }
